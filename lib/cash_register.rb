75067a8577dd8d60c42047cb500483856e646aea
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-10-31 16:10:38 +0300

    Completed Assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,9 @@
-
+class CashRegister
+  attr_accessor :discount, :total, :items, :last_transaction
+  def initialization (discount=0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
+  def 
+end
